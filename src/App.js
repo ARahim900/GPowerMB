ae21b0961405c0b424c54d118a9d63a03493ff73
Abdularahim Al Balushi <143422547+ARahim900@users.noreply.github.com>
2025-05-20 22:49:07 +0400

    Update App.js to include ElectricityAnalysis route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Dashboard from './pages/Dashboard';
-import WaterAnalysis from './pages/WaterAnalysis';
-import ElectricityAnalysis from './pages/ElectricityAnalysis';
-import STPPlant from './pages/STPPlant';
 import ContractorTracker from './pages/ContractorTracker';
 import ContractorDetail from './pages/ContractorDetail';
 import ContractorFinance from './pages/ContractorFinance';
+import WaterAnalysis from './pages/WaterAnalysis';
+import ElectricityAnalysis from './pages/ElectricityAnalysis';
+import STPPlant from './pages/STPPlant';
 
 function App() {
   return (
@@ -14,12 +14,12 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
-          <Route path="water-analysis" element={<WaterAnalysis />} />
-          <Route path="electricity-analysis" element={<ElectricityAnalysis />} />
-          <Route path="stp-plant" element={<STPPlant />} />
-          <Route path="contractor-tracker" element={<ContractorTracker />} />
-          <Route path="contractor-tracker/:id" element={<ContractorDetail />} />
-          <Route path="contractor-finance" element={<ContractorFinance />} />
+          <Route path="contractors" element={<ContractorTracker />} />
+          <Route path="contractors/:id" element={<ContractorDetail />} />
+          <Route path="contractors/finance" element={<ContractorFinance />} />
+          <Route path="water" element={<WaterAnalysis />} />
+          <Route path="electricity" element={<ElectricityAnalysis />} />
+          <Route path="stp" element={<STPPlant />} />
         </Route>
       </Routes>
     </Router>
