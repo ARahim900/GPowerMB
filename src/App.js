7d1540847913cab59739ef5617579a36aa041808
Abdularahim Al Balushi <143422547+ARahim900@users.noreply.github.com>
2025-05-20 18:56:13 +0400

    Add App.js with routing configuration

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import Dashboard from './pages/Dashboard';
+import WaterAnalysis from './pages/WaterAnalysis';
+import ElectricityAnalysis from './pages/ElectricityAnalysis';
+import STPPlant from './pages/STPPlant';
+import ContractorTracker from './pages/ContractorTracker';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="water-analysis" element={<WaterAnalysis />} />
+          <Route path="electricity-analysis" element={<ElectricityAnalysis />} />
+          <Route path="stp-plant" element={<STPPlant />} />
+          <Route path="contractor-tracker" element={<ContractorTracker />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
